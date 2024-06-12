535fa966ff0335dedf37ce032649282bbcde0ce8
Mobin Valikhani <103781147+M0BIN-V@users.noreply.github.com>
2024-06-12 18:45:53 +0330

    Add project files.

---
+++ Validity.DataAnnotation/NestedObjectValidator.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Validity.DataAnnotation;
+
+public static class NestedObjectValidator
+{
+    public static List<ValidationResult> Validate(object obj)
+    {
+        var results = new List<ValidationResult>();
+
+        foreach (var innerObj in ObjectTraverser.GetInnerObjects(obj))
+        {
+            Validator.TryValidateObject(innerObj, new ValidationContext(innerObj), results, true);
+        }
+
+        return results;
+    }
+}
